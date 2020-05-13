b6a6045b8e2247bfb4c8650988988efb6a48bcb0
Steven Topazio <sat@northpointprinting.com>
2020-05-13 15:30:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ def assign_rooms(attendees)
   room=0 
   attendees.collect do |name|
     room+=1 
-    "Hello #{name}! You'll be assigned to room #{room}!"
+    "Hello, #{name}! You'll be assigned to room #{room}!"
   end
 end
 
